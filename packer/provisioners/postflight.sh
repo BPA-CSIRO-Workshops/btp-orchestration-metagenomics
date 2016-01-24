6170449f922760287ef215c2201975e1797c51cd
Jerico Revote <jerico.revote@gmail.com>
2016-01-25 10:56:22 +1100

    Added NX to postflight.

--- packer/provisioners/postflight.sh
+++ packer/provisioners/postflight.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-# Postflight
+# Desktop
 apt-get update
 apt-get install -y ubuntu-desktop
-apt-get install -y lightdm
-apt-get install -y gnome-session-fallback
-/usr/lib/lightdm/lightdm-set-defaults -s gnome-classic
-/usr/lib/lightdm/lightdm-set-defaults -l false
 /bin/sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+
+# NX
+cd /tmp
+wget -4 --no-check-certificate https://swift.rc.nectar.org.au:8888/v1/AUTH_809/NX/nomachine_5.0.63_1_amd64.deb
+dpkg -i nomachine_5.0.63_1_amd64.deb nomachine_5.0.63_1_amd64.deb
+rm nomachine_5.0.63_1_amd64.deb
 
 # Disable LTS Upgrade Notification
 sed -i 's/Prompt=lts/Prompt=never/g' /etc/update-manager/release-upgrades
