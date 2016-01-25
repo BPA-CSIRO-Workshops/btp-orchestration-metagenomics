10bb71ae2b5f99ce8eb31621960e7288130c0018
Jerico Revote <jerico.revote@gmail.com>
2016-01-25 23:50:48 +1100

    Ensure datasource is ec2 for postflight.

--- packer/provisioners/postflight.sh
+++ packer/provisioners/postflight.sh
@@ -11,19 +11,12 @@ wget -4 --no-check-certificate https://swift.rc.nectar.org.au:8888/v1/AUTH_809/N
 dpkg -i nomachine_5.0.63_1_amd64.deb
 rm nomachine_5.0.63_1_amd64.deb
 
-# Clean up udev rules to prevent incrementing network card IDs
-if [ -f /etc/udev/rules.d/70-persistent-net.rules ]; then
-    rm /etc/udev/rules.d/70-persistent-net.rules
-    touch /etc/udev/rules.d/70-persistent-net.rules
-fi
-
-[ -d /dev/.udev ] && rm -rf /dev/.udev
+# cloud-init
+echo 'datasource_list: [ Ec2 ]' | sudo -s tee /etc/cloud/cloud.cfg.d/90_dpkg.cfg
+dpkg-reconfigure -f noninteractive cloud-init
 
 # Empty log files on the VM
 find /var/log -type f -execdir truncate -s0 {} \;
-
-# Remove SSH host keys
-rm -f /etc/ssh/*key*
 
 # Disable LTS Upgrade Notification
 sed -i 's/Prompt=lts/Prompt=never/g' /etc/update-manager/release-upgrades
