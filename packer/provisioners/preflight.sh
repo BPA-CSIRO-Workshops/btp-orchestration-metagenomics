262e194da7b06f03415995e312b873a8a38e1902
Jerico Revote <jerico.revote@gmail.com>
2016-01-25 20:01:15 +1100

    Added extra cleaning stuff.

--- packer/provisioners/preflight.sh
+++ packer/provisioners/preflight.sh
@@ -5,7 +5,7 @@ rm -r /var/lib/apt/lists/*
 apt-get update
 apt-get upgrade -y
 apt-get dist-upgrade -y
-apt-get autoremove -y;
+apt-get autoremove -y
 apt-get clean
 echo "precedence ::ffff:0:0/96 100" >> /etc/gai.conf
 
