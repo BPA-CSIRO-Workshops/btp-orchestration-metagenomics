25197132e42afcb9f94beec21085ade3584facb2
Jerico Revote <jerico.revote@gmail.com>
2016-06-07 10:16:48 +1000

    Update base image for NeCTAR to be Trusty. Setup MATE desktop on image.

--- packer/provisioners/desktop.sh
+++ packer/provisioners/desktop.sh
@@ -4,3 +4,10 @@
 apt-get update
 apt-get install -y ubuntu-desktop
 apt-get clean
+
+# MATE
+apt-add-repository -y ppa:ubuntu-mate-dev/ppa
+apt-add-repository -y ppa:ubuntu-mate-dev/trusty-mate
+apt-get update
+apt-get upgrade -y
+apt-get install -y ubuntu-mate-core ubuntu-mate-desktop
