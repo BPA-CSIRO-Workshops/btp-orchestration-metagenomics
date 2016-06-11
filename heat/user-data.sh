31be9ea21fda3c1af7f0801334dcf9b46279d932
Jerico Revote <jerico.revote@gmail.com>
2016-06-11 12:14:41 +1000

    Added IGV desktop shortcut too.

--- heat/user-data.sh
+++ heat/user-data.sh
@@ -35,23 +35,30 @@ done;
 # Create autostart dir for trainee user:
 if [ ! -e "/home/trainee/.config/autostart" ]; then
   mkdir -p /home/trainee/.config/autostart
-  cat > /home/trainee/.config/autostart/session-desktop << EOF
+fi
+
+cat > /home/trainee/.config/autostart/btp.desktop << EOF
 [Desktop Entry]
-Name=NGS-Nature
-GenericName=NGS-Nature
+Name=BTP-Script
+GenericName=BTP-Script
 Comment=NGS-Nature
-Exec=/home/trainee/.config/gsettings
+Exec=btp-custom
 Type=Application
+Icon=system-run
+Terminal=false
 X-GNOME-Autostart-enabled=true
 EOF
+
+if [ ! -e "/home/trainee/bin" ]; then
+  mkdir -p /home/trainee/bin
+  chown trainee.trainee /home/trainee/bin
 fi
 
 # Create gsettings desktop script
 # to customise trainee's desktop environment:
-if [ ! -f "/home/trainee/.config/gsettings" ]; then
-  cat > /home/trainee/.config/gsettings << EOF
+if [ ! -f "/home/trainee/bin/btp-custom" ]; then
+  cat > /home/trainee/bin/btp-custom << EOF
 #!/bin/bash
-
 gsettings set org.gnome.desktop.background picture-uri "file:///usr/share/backgrounds/ubuntu-gnome/ubuntu-gnome-wonders-of-nature.jpg"
 gsettings set org.gnome.desktop.lockdown disable-lock-screen true
 gsettings set org.gnome.desktop.session idle-delay 0
@@ -61,7 +68,21 @@ gsettings set org.gnome.shell.overrides button-layout ":minimize,maximize,close"
 gsettings set org.gnome.login-screen disable-restart-buttons true
 gsettings set org.gnome.shell enabled-extensions "['window-list@gnome-shell-extensions.gcampax.github.com']"
 EOF
+  chmod a+x /home/trainee/bin/btp-custom
 fi
+
+# Setup desktop shortcuts:
+if [ ! -e "/home/trainee/Desktop/" ]; then
+  mkdir /home/trainee/Desktop/
+fi
+cp /usr/share/applications/gnome-terminal.desktop /home/trainee/Desktop/
+cp /usr/share/applications/firefox.desktop /home/trainee/Desktop/
+cp /usr/share/applications/gedit.desktop /home/trainee/Desktop/
+cp /usr/share/applications/libreoffice-calc.desktop /home/trainee/Desktop/
+cp /usr/share/applications/libreoffice-write.desktop /home/trainee/Desktop/
+cp /usr/share/applications/IGV.desktop /home/trainee/Desktop/
+chmod a+x -R /home/trainee/Desktop
+chown trainee.trainee -R /home/trainee/Desktop
 
 # Verify trainee ownership of config dir:
 chown trainee.trainee -R /home/trainee/.config
